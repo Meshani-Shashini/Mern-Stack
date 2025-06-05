5c98b71da33965aaf7a585333fd359cb15d312e6
Meshani <msshani1209@gmail.com>
2025-06-05 23:57:38 +0530

    delete node modules

--- mern-auth/server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { verifyToken } from '../middleware/userAuth.js';
-import { getUserData } from '../controllers/userController.js';
-
-const userRouter = express.Router();
-
-userRouter.get('/data', verifyToken, getUserData); // Protect route with verifyToken middleware
-
-export default userRouter;
